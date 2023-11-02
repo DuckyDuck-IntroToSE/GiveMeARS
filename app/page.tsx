fdac81b6184a61d6572f1e296166ffedf87f866a
Hai Tuyen <nguyenhaituyen.github@gmail.com>
2023-11-02 09:34:06 +0700

    update link in root page.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { siteConfig } from "../config/site";
+import { title, subtitle } from "../components/primitives";
+import { GithubIcon } from "../components/icons";
 
 export default function Home() {
 	return (
