5507a2679115839a1ecdc53a6ae5f01474aafce4
Hai Tuyen <nguyenhaituyen.github@gmail.com>
2023-11-13 23:34:25 +0700

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col">
 						<Navbar />
-						<main className="container mx-auto max-w-7xl px-6 flex-grow">
+						<main className="container mx-auto max-w-7xl px-6">
 							{children}
 						</main>
 						<Footer></Footer>
